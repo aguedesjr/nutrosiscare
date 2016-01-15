5016c9b797ed99563e9aba5a934e5bf987918d8d
Antonio Guedes <Antonio Guedes@192.168.25.25>
2016-01-15 01:08:53 -0200

    Ajuste da edição do paciente

--- editarpaciente.php
+++ editarpaciente.php
@@ -166,9 +166,43 @@ $datan = implode("/", array_reverse(explode("-", $resulteditpac[1])));
                                 </td><td bgcolor="#FDFDFD"></td><td bgcolor="#FDFDFD"></td><td bgcolor="#FDFDFD"></td><td bgcolor="#FDFDFD"></td><td bgcolor="#FDFDFD"></td><td bgcolor="#FDFDFD"></td><td bgcolor="#FDFDFD"></td>
                                 <td bgcolor="#FDFDFD">
                                     <label>UF</label>
-                                    <div class="input-control text size1" data-role="input-control">
+                                    <div class="input-control select">
+                                                    <select name="estado">
+                                                        <option value=<? echo utf8_encode($resulteditpac[10])?>><? echo utf8_encode($resulteditpac[10])?></option>
+                                                        <option value=""></option>
+                                                        <option value="AC">AC</option> 
+														<option value="AL">AL</option> 
+														<option value="AM">AM</option> 
+														<option value="AP">AP</option> 
+														<option value="BA">BA</option> 
+														<option value="CE">CE</option> 
+														<option value="DF">DF</option> 
+														<option value="ES">ES</option> 
+														<option value="GO">GO</option> 
+														<option value="MA">MA</option> 
+														<option value="MT">MT</option> 
+														<option value="MS">MS</option> 
+														<option value="MG">MG</option> 
+														<option value="PA">PA</option> 
+														<option value="PB">PB</option> 
+														<option value="PR">PR</option> 
+														<option value="PE">PE</option> 
+														<option value="PI">PI</option> 
+														<option value="RJ">RJ</option> 
+														<option value="RN">RN</option> 
+														<option value="RO">RO</option> 
+														<option value="RS">RS</option> 
+														<option value="RR">RR</option> 
+														<option value="SC">SC</option> 
+														<option value="SE">SE</option> 
+														<option value="SP">SP</option> 
+														<option value="TO">TO</option>  
+                                                    </select>
+                                                </div>
+                                    
+                                    <!-- <div class="input-control text size1" data-role="input-control">
                                         <input type="text" id="estado" name="estado" value="<? echo utf8_encode($resulteditpac[10])?>" placeholder="Estado">
-                                    </div>
+                                    </div>-->
                                 </td> 
                             </tr></table><br />
                             <center>
