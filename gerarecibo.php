9c90d4a6728fa8164f02e57a93af3e0a38a24fe4
Antonio Guedes <Antonio Guedes@192.168.25.25>
2016-01-16 17:09:42 -0200

    Inicio da geração de recibo

---
+++ gerarecibo.php
@@ -0,0 +1,32 @@
+<?
+session_start();
+//Requer autenticação
+require_once ("validalogin.php");
+//Requer conexão previa com o banco
+require_once ("configs/conn.php");
+//Classe usada para gerar o arquivo pdf
+require_once ("fpdf16/fpdf.php");
+//Classe usada para gerar o arquivo pdf
+require_once ("fpdi/fpdi.php");
+//Caminho do arquivo de fontes para o FPDF
+define('FPDF_FONTPATH','fpdf16/font/');
+
+// initiate FPDI
+$pdf = new FPDI();
+// add a page
+$pdf->AddPage();
+// set the source file
+$pdf->setSourceFile("fpdi/timbre.pdf");
+// import page 1
+$tplIdx = $pdf->importPage(1);
+// use the imported page and place it at point 10,10 with a width of 100 mm
+$pdf->useTemplate($tplIdx, 10, 10, 100);
+
+// now write some text above the imported page
+$pdf->SetFont('Helvetica');
+$pdf->SetTextColor(255, 0, 0);
+$pdf->SetXY(30, 30);
+$pdf->Write(0, 'This is just a simple text');
+
+$pdf->Output();
+?>
