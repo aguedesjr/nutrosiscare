a24ce55c85581b2d90e8ef52c6526db53fb13994
Antonio Guedes <Antonio Guedes@192.168.25.25>
2016-01-16 21:07:06 -0200

    Ajustes do recibo

--- gerarecibo.php
+++ gerarecibo.php
@@ -24,9 +24,9 @@ $pdf->useTemplate($tplIdx, 0, 0, 210);
 
 // now write some text above the imported page
 $pdf->SetFont('Arial','U',10);
-$pdf->Ln(40);
+$pdf->Ln(60);
 $pdf->Cell(20);
-$pdf->MultiCell(100,10,'This is just a simple text',1,J);
+$pdf->MultiCell(180,10,'This is just a simple text',1,J);
 
 $pdf->Output("recibo.pdf",D);
 ?>
