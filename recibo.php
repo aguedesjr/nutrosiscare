34ebb9af4b3fdaa53848f574ad290c08683a23ca
Antonio Guedes <Antonio Guedes@192.168.25.25>
2016-01-16 15:41:42 -0200

    Adição de cpf ao recibo

--- recibo.php
+++ recibo.php
@@ -5,6 +5,105 @@ include ("header.php");
 
 ?>
 
+<script src="js/jquery.maskedinput.js" type="text/javascript"></script> <!-- SCRIPT MASK -->
+
+<script>
+        $(function() {
+            jQuery(function($){
+                $("#cpf").mask("999.999.999-99");
+                
+            });
+        });
+        
+        //function zerar() {
+          //  document.getElementById("divcpf").className = "input-control text";
+          //  document.getElementById("divnome").className = "input-control text";
+          //  document.getElementById("salvapaciente").reset();
+            
+        //}
+        
+        function valida(form) {
+            //if (form.nome.value=="") {
+                //alert("Nome não informado!!");
+                //document.getElementById("divnome").className = "input-control text error-state";
+                //form.nome.focus();
+                //return false;
+            //}
+            
+            //if (form.nome.value!="") {
+                //document.getElementById("divnome").className = "input-control text";
+            //}
+            
+            var str = document.getElementById("cpf").value;
+            
+            //if (form.cpf.value=="") {
+                //alert("CPF não informado!!");
+                //document.getElementById("divcpf").className = "input-control text error-state";
+                //form.cpf.focus();
+                //return false;
+            //} 
+            if (form.cpf.value!="") {
+                str = str.replace('.','');
+                str = str.replace('.','');
+                str = str.replace('-','');
+                cpf = str;
+                var numeros, digitos, soma, i, resultado, digitos_iguais;
+                digitos_iguais = 1;
+                if (cpf.length < 11) {
+                      alert("CPF invalido!!");
+                      document.getElementById("divcpf").className = "input-control text error-state";
+                      form.cpf.focus();
+                      return false;
+                }
+                for (i = 0; i < cpf.length - 1; i++)
+                      if (cpf.charAt(i) != cpf.charAt(i + 1))
+                            {
+                            digitos_iguais = 0;
+                            //alert("CPF invalido!!");
+                            document.getElementById("divcpf").className = "input-control text error-state";
+                            form.cpf.focus();
+                            break;
+                            }
+                if (!digitos_iguais)
+                      {
+                      numeros = cpf.substring(0,9);
+                      digitos = cpf.substring(9);
+                      soma = 0;
+                      for (i = 10; i > 1; i--)
+                            soma += numeros.charAt(10 - i) * i;
+                      resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                      if (resultado != digitos.charAt(0)) {
+                            alert("CPF invalido!!");
+                            document.getElementById("divcpf").className = "input-control text error-state";
+                            form.cpf.focus();
+                            return false;
+                      }
+                      numeros = cpf.substring(0,10);
+                      soma = 0;
+                      for (i = 11; i > 1; i--)
+                            soma += numeros.charAt(11 - i) * i;
+                      resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+                      if (resultado != digitos.charAt(1)){
+                            alert("CPF invalido!!");
+                            document.getElementById("divcpf").className = "input-control text error-state";
+                            form.cpf.focus();
+                            return false;
+                        }
+                      document.getElementById("divcpf").className = "input-control text";
+                      return true;
+                      }
+                else {
+                    alert("CPF invalido!!");
+                    document.getElementById("divcpf").className = "input-control text error-state";
+                    form.cpf.focus();
+                    return false;
+                }
+                
+            }
+       };
+        
+    </script>
+
 <!-- INICIO DO ARQUIVO -->
 <body class="metro">
     
@@ -38,6 +137,12 @@ include ("header.php");
 					                            <div class="input-control text" id="divnome" data-role="input-control">
 					                                <input type="text" id="nome" name="nome" placeholder="Nome do Paciente" autofocus required>
 					                            </div>
+                                    		</td>
+                                    		<td bgcolor="#FDFDFD">
+                                    			<label>CPF</label>
+				                                <div class="input-control text size2" id="divcpf" data-role="input-control">
+				                                    <input type="text" id="cpf" name="cpf" maxlength="14" placeholder="Informe o CPF">
+				                                </div>
                                     		</td>
                                     	</tr>
                                     </table><br>
