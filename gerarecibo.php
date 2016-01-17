3efc3eae94d9bb5c2a19cf432d46a7ace10977f0
Antonio Guedes <Antonio Guedes@192.168.25.25>
2016-01-17 16:02:45 -0200

    Ajustando assinatura

--- gerarecibo.php
+++ gerarecibo.php
@@ -56,7 +56,7 @@ $pdf->SetFont('Arial','',10);
 $pdf->Ln(20);
 $pdf->Cell(20);
 $pdf->MultiCell(150,10,'Recebi de '.$nome.', CPF '.$cpf.', a importância de R$'.$valor.' ('.$valorex.') referente à consulta médica.',0,J);
-$pdf->Ln(140);
+$pdf->Ln(135);
 $pdf->Cell(45);
 $pdf->Cell(20,10,'_____________________________________________________');
 $pdf->Ln(5);
