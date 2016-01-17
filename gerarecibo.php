81e88583b5af85e6271e95bffcd188e6fb9f79f0
Antonio Guedes <Antonio Guedes@192.168.25.25>
2016-01-17 16:14:24 -0200

    Ajustando layout

--- gerarecibo.php
+++ gerarecibo.php
@@ -60,7 +60,7 @@ $pdf->SetFont('Arial','',10);
 $pdf->Ln(20);
 $pdf->Cell(20);
 $pdf->MultiCell(150,10,'Recebi de '.$nome.', CPF '.$cpf.', a importância de R$'.$valor.' ('.$valorex.') referente à consulta médica.',0,J);
-$pdf->Ln(135);
+$pdf->SetY(200);
 $pdf->Cell(45);
 $pdf->Cell(20,10,'_____________________________________________________');
 $pdf->Ln(5);
