fccc18bb9057c723b2326269456927c5b71da2d9
Antonio Guedes <Antonio Guedes@onyxpc>
2016-03-03 22:23:56 -0300

    Removido a imagem do nome do usuário.
    Finalizado a ficha receituario.

--- fichareceituario.php
+++ fichareceituario.php
@@ -113,11 +113,10 @@ $resultpac = mysql_fetch_array($resultadopac);
                                     <a class="button image-button primary image-left" name="buscarPaciente" href="buscarpacientereceituario.php"><i class="icon-search on-left" style="top: -3px; left: 7px"></i>Buscar</a>
                                 </td> 
                             </tr></table>
-                            <input type="hidden" id="id" name="id" value="<? echo $resultpac[1];?>"> <!-- Envia o id do paciente -->
                             <label>Nome</label>
                             <div class="input-control text" data-role="input-control">
-                            	<span class="mif-user prepend-icon"></span>
                                 <input type="text" id="nome" name="nome" disabled="disabled" value="<? echo utf8_encode($resultpac[0])?>" placeholder="Nome do Paciente">
+                                <input type="hidden" id="id" name="id" value="<? echo $resultpac[1];?>"> <!-- Envia o id do paciente -->
                             </div>
                             <table><tr>
                             <td bgcolor="#FDFDFD">
